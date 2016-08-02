42d155c7d06f618e1200287499f199b23191a3a4
lentan1029 <lentan1029@gmail.com>
2016-08-02 11:43:36 -0700

    Add Some Pictures

--- src/pseudoclassical-movingDancer.js
+++ src/pseudoclassical-movingDancer.js
@@ -1,9 +1,14 @@
 var MovingDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, 1);
+  var colors = ['blue', 'red', 'green'];
+  var color = colors[Math.floor(Math.random() * 3)];
+  console.log(color);
+  console.log('<span class="dancer moving"><img src="./' + color + 'mobius.png"></img></span>');
+  this.$node = $('<span class="dancer moving"><img src="./' + color + 'mobius.png"></img></span>');
   this.$node.addClass('moving');
-  this.horizontalSpeed = Math.random() * 10 + 5 ;
+  this.horizontalSpeed = Math.random() * 10 + 5;
   this.verticalSpeed = Math.random() * 10 + 5;
-  this.padding = 5;
+  this.padding = 0;
 };
 
 MovingDancer.prototype = Object.create(Dancer.prototype);
@@ -27,13 +32,13 @@ MovingDancer.prototype.move = function() {
   this.verticalSpeed = this.verticalSpeed * 0.999;
   this.horizontalSpeed = this.horizontalSpeed * 0.999;
   this.setPosition(this.top + this.verticalSpeed, this.left + this.horizontalSpeed);
-  if (this.left + 120 > $('body').width() - this.padding) { //exiting right border
+  if (this.left + this.$node.width() > $('body').width() - this.padding) { //exiting right border
     this.horizontalSpeed = -Math.abs(this.horizontalSpeed);
   }
   if (this.left < this.padding) {
     this.horizontalSpeed = Math.abs(this.horizontalSpeed);
   }
-  if (this.top + 120 > $('body').height() - this.padding) { //exiting bottom border
+  if (this.top + this.$node.height() > $('body').height() - this.padding) { //exiting bottom border
     this.verticalSpeed = -Math.abs(this.verticalSpeed);
   }
   if (this.top < this.padding) {
