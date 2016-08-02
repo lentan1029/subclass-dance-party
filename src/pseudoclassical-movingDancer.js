99f1d31320956812a84a818cfa469c166f366088
lentan <lentan1029@gmail.com>
2016-08-02 15:56:38 -0700

    Make Gravity

--- src/pseudoclassical-movingDancer.js
+++ src/pseudoclassical-movingDancer.js
@@ -1,9 +1,9 @@
 var MovingDancer = function(top, left, timeBetweenSteps) {
-  Dancer.call(this, top, left, 1);
+  Dancer.call(this, top, left, 5);
   var colors = ['blue', 'red', 'green'];
   var color = colors[Math.floor(Math.random() * 3)];
-  console.log(color);
-  console.log('<span class="dancer moving"><img src="./' + color + 'mobius.png"></img></span>');
+  // console.log(color);
+  // console.log('<span class="dancer moving"><img src="./' + color + 'mobius.png"></img></span>');
   this.$node = $('<span class="dancer moving"><img src="./' + color + 'mobius.png"></img></span>');
   this.$node.addClass('moving');
   this.horizontalSpeed = Math.random() * 10 + 5;
@@ -29,8 +29,30 @@ MovingDancer.prototype.step = function(timeBetweenSteps) {
 };
 
 MovingDancer.prototype.move = function() {
-  this.verticalSpeed = this.verticalSpeed * 0.999;
-  this.horizontalSpeed = this.horizontalSpeed * 0.999;
+  var verticalDistFromBlackhole = this.$node.position().top - $('#blackhole').position().top;
+  var horizontalDistFromBlackhole = this.$node.position().left - $('#blackhole').position().left;
+  console.log(verticalDistFromBlackhole);
+  // console.log(this.$node.position().top);
+  var absDist = Math.pow(verticalDistFromBlackhole, 2) + Math.pow(horizontalDistFromBlackhole, 2);
+  var factor = 2000 / absDist;
+
+  this.verticalSpeed = this.verticalSpeed * 0.9935;
+  if (verticalDistFromBlackhole < -30) {
+    this.verticalSpeed += Math.abs(Math.sin(Math.atan(verticalDistFromBlackhole / horizontalDistFromBlackhole))) * factor;
+  } else if (verticalDistFromBlackhole > 30) {
+    this.verticalSpeed -= Math.abs(Math.sin(Math.atan(verticalDistFromBlackhole / horizontalDistFromBlackhole))) * factor;
+  }
+  this.horizontalSpeed = this.horizontalSpeed * 0.9935;
+  if (horizontalDistFromBlackhole < -30) {
+    this.horizontalSpeed += Math.abs(Math.cos(Math.atan(verticalDistFromBlackhole / horizontalDistFromBlackhole))) * factor;
+  } else if (horizontalDistFromBlackhole > 30) {
+    this.horizontalSpeed -= Math.abs(Math.cos(Math.atan(verticalDistFromBlackhole / horizontalDistFromBlackhole))) * factor;
+  }
+  // if (horizontalDistFromBlackhole < -1) {
+  //   this.horizontalSpeed += Math.cos(Math.atan(verticalDistFromBlackhole / horizontalDistFromBlackhole)) * factor;
+  // } else if (horizontalDistFromBlackhole > 1) {
+  //   this.horizontalSpeed -= Math.cos(Math.atan(verticalDistFromBlackhole / horizontalDistFromBlackhole)) * factor;
+  // }
   this.setPosition(this.top + this.verticalSpeed, this.left + this.horizontalSpeed);
   if (this.left + this.$node.width() > $('body').width() - this.padding) { //exiting right border
     this.horizontalSpeed = -Math.abs(this.horizontalSpeed);
