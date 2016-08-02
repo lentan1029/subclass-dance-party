0ac0e12e1464766e81fff64e574ceab59edde6fe
lentan1029 <lentan1029@gmail.com>
2016-08-02 15:57:53 -0700

    Changes Made to Website

--- src/pseudoclassical-movingDancer.js
+++ src/pseudoclassical-movingDancer.js
@@ -1,5 +1,5 @@
 var MovingDancer = function(top, left, timeBetweenSteps) {
-  Dancer.call(this, top, left, 1);
+  Dancer.call(this, top, left, .1);
   var colors = ['blue', 'red', 'green'];
   var color = colors[Math.floor(Math.random() * 3)];
   console.log(color);
@@ -29,8 +29,27 @@ MovingDancer.prototype.step = function(timeBetweenSteps) {
 };
 
 MovingDancer.prototype.move = function() {
-  this.verticalSpeed = this.verticalSpeed * 0.999;
-  this.horizontalSpeed = this.horizontalSpeed * 0.999;
+  debugger;
+  var difftop = $('#blackhole').position().top - this.$node.position().top;
+  var diffleft = $('#blackhole').position().left - this.$node.position().left;
+  var magdist = Math.pow(difftop, 2) + Math.pow(diffleft, 2);
+  var magvel = Math.pow(this.verticalSpeed, 2) + Math.pow(this.horizontalSpeed, 2);
+  var factor = 1000 / (magdist + 1000 * magvel);
+
+  var distangle = Math.atan2(-difftop, diffleft);
+  var dancerangle = Math.atan2(-this.verticalSpeed, this.horizontalSpeed);
+  var newangle = dancerangle + factor * distangle;
+
+  this.horizontalSpeed = .999 * Math.cos(newangle) * Math.pow(magvel, .5);
+  this.verticalSpeed = .999 * (Math.sin(newangle) * Math.pow(magvel, .5));
+
+  //console.log(this.$node.position().top);
+  // this.verticalSpeed += Math.sin(Math.atan(difftop/diffleft)) * factor;
+  // this.verticalSpeed *= 0.99;
+  // this.horizontalSpeed += Math.cos(Math.atan(difftop/diffleft)) * factor;
+  // this.horizontalSpeed *= 0.99;
+
+
   this.setPosition(this.top + this.verticalSpeed, this.left + this.horizontalSpeed);
   if (this.left + this.$node.width() > $('body').width() - this.padding) { //exiting right border
     this.horizontalSpeed = -Math.abs(this.horizontalSpeed);
